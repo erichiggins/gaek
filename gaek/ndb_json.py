3df68d48bd59b6b869cf162a02089598ad74c682
Eric Higgins <erichiggins@gmail.com>
2017-02-02 16:47:02 -0800

    Add internal caching. Hardcode instead of compute. Functions instead of obj methods. RegEx instead of loose date-string checks.

--- gaek/ndb_json.py
+++ gaek/ndb_json.py
@@ -34,9 +34,11 @@ __email__ = 'erichiggins@gmail.com'
 import base64
 import datetime
 import json
+import re
 import time
 import types
 
+import arrow
 import dateutil.parser
 from google.appengine.ext import ndb
 
@@ -48,6 +50,9 @@ __all__ = (
     'NdbDecoder',
     'NdbEncoder',
 )
+
+
+DATE_RE = re.compile(r'\d{4}[-/]\d{2}[-/]\d{2}')
 
 
 def encode_model(obj):
@@ -131,7 +136,38 @@ NDB_TYPE_ENCODING = {
 }
 
 # Sort the types so any iteration is in a deterministic order
-NDB_TYPES = sorted(NDB_TYPE_ENCODING.keys(), key=lambda t: t.__name__)
+NDB_TYPES = (
+  ndb.Future,
+  ndb.Key,
+  ndb.MetaModel,
+  ndb.Query,
+  ndb.QueryIterator,
+  ndb.model._BaseValue,
+  types.ComplexType,
+  datetime.date,
+  datetime.datetime,
+  time.struct_time,
+)
+
+
+def _object_hook_handler(val):
+  """Handles decoding of nested date strings."""
+  return {k: _decode_date(v) for k, v in val.iteritems()}
+
+
+def _decode_date(val):
+  """Tries to decode strings that look like dates into datetime objects."""
+  if isinstance(val, basestring) and DATE_RE.match(val):
+    try:
+      dt = arrow.parser.DateTimeParser().parse_iso(val)
+      # Check for UTC.
+      if val.endswith(('+00:00', '-00:00', 'Z')):
+        # Then remove tzinfo for gae, which is offset-naive.
+        dt = dt.replace(tzinfo=None)
+      return dt
+    except (TypeError, ValueError):
+      pass
+  return val
 
 
 class NdbDecoder(json.JSONDecoder):
@@ -139,30 +175,12 @@ class NdbDecoder(json.JSONDecoder):
 
   def __init__(self, **kwargs):
     """Override the default __init__ in order to specify our own parameters."""
-    json.JSONDecoder.__init__(self, object_hook=self.object_hook_handler, **kwargs)
-
-  def object_hook_handler(self, val):
-    """Handles decoding of nested date strings."""
-    return {k: self.decode_date(v) for k, v in val.iteritems()}
-
-  def decode_date(self, val):
-    """Tries to decode strings that look like dates into datetime objects."""
-    if isinstance(val, basestring) and val.count('-') == 2 and len(val) > 9:
-      try:
-        dt = dateutil.parser.parse(val)
-        # Check for UTC.
-        if val.endswith(('+00:00', '-00:00', 'Z')):
-          # Then remove tzinfo for gae, which is offset-naive.
-          dt = dt.replace(tzinfo=None)
-        return dt
-      except (TypeError, ValueError):
-        pass
-    return val
+    json.JSONDecoder.__init__(self, object_hook=_object_hook_handler, **kwargs)
 
   def decode(self, val):
     """Override of the default decode method that also uses decode_date."""
     # First try the date decoder.
-    new_val = self.decode_date(val)
+    new_val = _decode_date(val)
     if val != new_val:
       return new_val
     # Fall back to the default decoder.
@@ -172,47 +190,46 @@ class NdbDecoder(json.JSONDecoder):
 class NdbEncoder(json.JSONEncoder):
   """Extend the JSON encoder to add support for NDB Models."""
 
-
   def __init__(self, **kwargs):
     self._ndb_type_encoding = NDB_TYPE_ENCODING.copy()
+    self._cached_type_encoding = {}
 
     keys_as_entities = kwargs.pop('ndb_keys_as_entities', False)
     keys_as_pairs = kwargs.pop('ndb_keys_as_pairs', False)
     keys_as_urlsafe = kwargs.pop('ndb_keys_as_urlsafe', False)
 
-    # Validate that only one of three flags is True
-    if ((keys_as_entities and keys_as_pairs)
-        or (keys_as_entities and keys_as_urlsafe)
-        or (keys_as_pairs and keys_as_urlsafe)):
-      raise ValueError('Only one of arguments ndb_keys_as_entities, ndb_keys_as_pairs, ndb_keys_as_urlsafe can be True')
+    if any((keys_as_entities, keys_as_pairs, keys_as_urlsafe)):
+      # Validate that only one of three flags is True
+      if ((keys_as_entities and keys_as_pairs)
+          or (keys_as_entities and keys_as_urlsafe)
+          or (keys_as_pairs and keys_as_urlsafe)):
+        raise ValueError('Only one of arguments ndb_keys_as_entities, ndb_keys_as_pairs, ndb_keys_as_urlsafe can be True')
 
-    if keys_as_pairs:
-      self._ndb_type_encoding[ndb.Key] = encode_key_as_pair
-    elif keys_as_urlsafe:
-      self._ndb_type_encoding[ndb.Key] = encode_key_as_urlsafe
-    else:
-      self._ndb_type_encoding[ndb.Key] = encode_key_as_entity
-
+      if keys_as_pairs:
+        self._ndb_type_encoding[ndb.Key] = encode_key_as_pair
+      elif keys_as_urlsafe:
+        self._ndb_type_encoding[ndb.Key] = encode_key_as_urlsafe
+      else:
+        self._ndb_type_encoding[ndb.Key] = encode_key_as_entity
 
     json.JSONEncoder.__init__(self, **kwargs)
 
   def default(self, obj):
     """Overriding the default JSONEncoder.default for NDB support."""
     obj_type = type(obj)
-    # NDB Models return a repr to calls from type().
-    if obj_type not in self._ndb_type_encoding:
-      if hasattr(obj, '__metaclass__'):
-        obj_type = obj.__metaclass__
-      else:
-        # Try to encode subclasses of types
-        for ndb_type in NDB_TYPES:
-          if isinstance(obj, ndb_type):
-            obj_type = ndb_type
-            break
+    fn = self._ndb_type_encoding.get(obj_type) or self._cached_type_encoding.get(obj_type) or self._ndb_type_encoding.get(getattr(obj, '__metaclass__', None))
+    if fn:
+      return fn(obj)
+
+    # Try to encode subclasses of types
+    for ndb_type in NDB_TYPES:
+      if isinstance(obj, ndb_type):
+        obj_type = ndb_type
+        break
 
     fn = self._ndb_type_encoding.get(obj_type)
-
     if fn:
+      self._cached_type_encoding[getattr(obj, '__metaclass__', type(obj))] = fn
       return fn(obj)
 
     return json.JSONEncoder.default(self, obj)
