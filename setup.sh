80197a5cf4446fdf2818454c3a362f6eed7997ac
Eric Higgins <erichiggins@gmail.com>
2020-01-25 00:36:12 -0800

    Release 0.4.1 (#22)
    * Added support for JSON encoding of subclasses of supported types
    
    * Sorted the ndb types for deterministic iteration
    
    * Add a flag fetch_models to specify the way to encode NDB keys (#11)
    
    * Add a flag fetch_models to specify the way to encode NDB keys
    
    * Keep backward compatibility
    
    * Introduced flags ndb_keys_as_entities, ndb_keys_as_pairs, ndb_keys_as_urlsafe
    
    * Covered with unit tests
    
    * Rebased with the latest development branch and simplified the code
    
    * Fixed documentation and test dependencies
    
    * Fixing the key error
    
    * Add a test that is_default_version() returns False
    
    * Fix get_current_module_name
    
    * All done I think
    
    * Cleaning things up a bit
    
    * fix typo
    
    * remove whitespace
    
    * Bump version to 0.4.0 for release.
    
    * Try experimental branch to fix deployment to Pypi
    
    * Upgrade pyyaml for security (#21)
    
    * Upgrade pyyaml req for security issue
    
    * Fix module imports for tests
    
    * Investigate virtualenv paths
    
    * More logging
    
    * List user site packages dir
    
    * More logging of site package paths
    
    * Symlink in another dir for travis
    
    * Clean up
    
    * bump version, update changelog
    
    Co-authored-by: Roman Onufryk <roman@optimizely.com>

--- setup.sh
+++ setup.sh
@@ -16,10 +16,16 @@ curl -O https://storage.googleapis.com/appengine-sdks/featured/$GAE_SDK_FILE
   
 echo "Verifying $GAE_SDK_FILE..."
 shasum $GAE_SDK_FILE
-  
+
 echo "Unzipping $GAE_SDK_FILE..."
 unzip -q $GAE_SDK_FILE -d .dev_env/
 rm $GAE_SDK_FILE
+
+# Travis CI uses a different path.
+cd .dev_env/site-packages/
+ln -s ../google_appengine/google google
+
+# OSX path.
 cd .dev_env/lib/python2.7/site-packages/
 ln -s ../../../google_appengine/google google
 cd ../../../../
--- tests/__init__.py
+++ tests/__init__.py
@@ -1,1 +1,10 @@
 # -*- coding: utf-8 -*-
+
+
+__all__ = []
+
+from test_environ import *
+__all__ += test_environ.__all__
+
+from test_ndb_json import *
+__all__ += test_ndb_json.__all__
